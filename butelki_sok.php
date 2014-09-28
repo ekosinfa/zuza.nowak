460274d9380650af1ed53223b650380d95a727d0
zuzanowak <zuzanna_nowak@ekos.edu.pl>
2014-09-28 17:37:43 +0200

    Create butelki_sok.php

---
+++ butelki_sok.php
@@ -0,0 +1,9 @@
+<?php
+for($i=99; $i >= 2; $i--){
+$zmienna= $i-1;
+print "$i butelek soku jabłkowego na ścianie, $i butelek soku. Weź jedną i podaj dalej, $zmienna butelek soku na na ścianie.\r\n";
+}
+print "1 butelka soku jabłkowego na ścianie, 1 butelka soku.Weż jedną i podaj dalej, nie ma więcej butelek soku na ścianie.\r\n";
+print "Nie ma więcej butelek soku na ścianie, nie ma więcej butelek soku. Idź do sklepu i kup więcej, 99 butelek soku na scianie.\r\n";
+?>
+
