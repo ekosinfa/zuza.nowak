958380b5a7e7112fa344c0692838e6f0cd9a3b5f
zuzanowak <zuzanna_nowak@ekos.edu.pl>
2014-10-14 07:50:25 +0200

    Update podatki.php

--- podatki.php
+++ podatki.php
@@ -1,20 +1,16 @@
 <?php
-
 $konta = array(
-
 "1"=>array(
 'imie'=>'Wioletta',
 'nazwisko'=>'Marcinek',
 'stan_konta'=>'6792zl',
 'przedmiot'=>'Polski'),
-
 "2"=>array(
 'imie'=>'Tomasz',
 'nazwisko'=>'Orka',
 'stan_konta' =>'99zl',
 'przedmiot'=>'Biologia',
 ),
-
 "3"=>array(
 'imie'=>'Marta',
 'nazwisko'=>'Stopa',
@@ -22,9 +18,10 @@ $konta = array(
 'przedmiot'=>'Matematyka'),
 );
 foreach ($konta as $konto){
-echo $konto ['imie'].'  ';
-echo $konto ['nazwisko'].'   ';
+echo $konto ['imie'].' ';
+echo $konto ['nazwisko'].' ';
 if($konto ['przedmiot']=='Polski'){echo $konto ['stan_konta']*0.23;}
-if($konto ['przedmiot']=='Matematyka'){echo $konto ['stan_konta']*0.23;}
-if($konto ['przedmiot']=='Biologia'){echo $konto ['stan_konta']*0.23;}
+if($konto ['przedmiot']=='Matematyka'){echo $konto ['stan_konta']*0.57;}
+if($konto ['przedmiot']=='Biologia'){echo $konto ['stan_konta']*0.08;}
 }
+?>
